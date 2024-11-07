35e4a157586970ea15cd4e98bccc8878986bea32
HondaYt <honda.yuto.04@gmail.com>
2024-11-08 01:43:59 +0900

    seederにデータを追加

--- 2024gorin-laravel/database/seeders/EventSeeder.php
+++ 2024gorin-laravel/database/seeders/EventSeeder.php
@@ -20,7 +20,17 @@ class EventSeeder extends Seeder
                 'title'=>'技能五輪',
                 'address'=>'愛知県名古屋市',
                 'event_date'=>'2024-11-06',
-            ]
+            ],
+            [
+                'title'=>'技能五輪2',
+                'address'=>'愛知県',
+                'event_date'=>'2024-11-07',
+            ],
+            [
+                'title'=>'技能五輪3',
+                'address'=>'愛知',
+                'event_date'=>'2024-11-08',
+            ],
         ]);
 
     }
