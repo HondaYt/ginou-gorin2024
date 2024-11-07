897ca63c5862521fe11393a82f0d3698c083fc82
HondaYt <honda.yuto.04@gmail.com>
2024-11-08 01:49:56 +0900

    routingを追加

--- 2024gorin-laravel/routes/web.php
+++ 2024gorin-laravel/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->group(function() {
+    // Route::get('/',[AdminController::class,'index'])->name('admin.index');
+    Route::resource('/',AdminController::class);
+    Route::resource('events',EventController::class);
 });
