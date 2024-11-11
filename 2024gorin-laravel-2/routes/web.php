25d72d904bb5bcb994e0c8b99b8f88a3a3cdbb94
HondaYt <honda.yuto.04@gmail.com>
2024-11-11 16:31:38 +0900

    admin/eventsへのRoutingを追加

--- 2024gorin-laravel-2/routes/web.php
+++ 2024gorin-laravel-2/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('admin/events', EventController::class);
