e91017d8bdc8e032765e1f84a8a3275d01b1b717
HondaYt <honda.yuto.04@gmail.com>
2024-11-11 15:58:30 +0900

    events tableのカラムを追加

--- 2024gorin-laravel-2/database/migrations/2024_11_11_154218_create_events_table.php
+++ 2024gorin-laravel-2/database/migrations/2024_11_11_154218_create_events_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 128);
+            $table->string('address', 255);
+            $table->date('event_date')->nullable();
             $table->timestamps();
         });
     }
