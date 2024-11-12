f81d4fcf52c465171ad9a61a2647120e3e1566f4
HondaYt <honda.yuto.04@gmail.com>
2024-11-13 01:18:08 +0900

    events tableにテストデータを追加。

--- 2024gorin-laravel-2/database/seeders/EventSeeder.php
+++ 2024gorin-laravel-2/database/seeders/EventSeeder.php
@@ -18,7 +18,22 @@ class EventSeeder extends Seeder
                 'title' => '技能五輪',
                 'address' => '愛知県名古屋市',
                 'event_date' => '2024-11-06',
-            ]
+            ],
+            [
+                'title' => '若年者ものづくり',
+                'address' => '群馬県',
+                'event_date' => '2024-07-10',
+            ],
+            [
+                'title' => 'クラス交流会',
+                'address' => '大阪府',
+                'event_date' => '2024-11-14',
+            ],
+            [
+                'title' => 'ECC EXPO',
+                'address' => '大阪府中崎町',
+                'event_date' => '2025-02-20',
+            ],
         ]);
     }
 }
