70925926e497e1a34464bd9a1f52db81798dc9c5
HondaYt <honda.yuto.04@gmail.com>
2024-11-18 00:38:36 +0900

    新規登録ページの表示を作成

--- 2024gorin-laravel-2/resources/views/admin/events/create.blade.php
+++ 2024gorin-laravel-2/resources/views/admin/events/create.blade.php
@@ -2,6 +2,22 @@
 
 @section('content') 
 <div>
-    This page is admin.event.create!!!
+    <form action="" method="POST">
+        @csrf
+
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="address">Address:</label>
+            <input type="text" name="address" id="address">
+        </div>
+        <div>
+            <label for="event_date">EventDate:</label>
+            <input type="text" name="event_date" id="event_date">
+        </div>
+        <button type="submit">登録</button>
+    </form>
 </div>
 @endsection
