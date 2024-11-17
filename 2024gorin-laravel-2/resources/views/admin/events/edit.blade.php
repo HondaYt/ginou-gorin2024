0f492c73c4e677365c98ed4779aed8daed1bb0e5
HondaYt <honda.yuto.04@gmail.com>
2024-11-18 01:04:35 +0900

    events.createを複製し、events.editを作成

---
+++ 2024gorin-laravel-2/resources/views/admin/events/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.layout')
+
+@section('content') 
+<div>
+    <form action="{{route('events.store')}}" method="POST">
+        @csrf
+
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="address">Address:</label>
+            <input type="text" name="address" id="address">
+        </div>
+        <div>
+            <label for="event_date">EventDate:</label>
+            <input type="text" name="event_date" id="event_date">
+        </div>
+        <button type="submit">登録</button>
+    </form>
+</div>
+@endsection
