78fe0433edb7582b42183cb8da40f69a84e70fc7
HondaYt <honda.yuto.04@gmail.com>
2024-11-19 16:36:14 +0900

    event apiのpost

--- 2024gorin-laravel-2/app/Http/Controllers/EventAPIController.php
+++ 2024gorin-laravel-2/app/Http/Controllers/EventAPIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\Dispatch;
 use Illuminate\Http\Request;
 
 class EventAPIController extends Controller
@@ -48,7 +49,31 @@ class EventAPIController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // 必須パラメータのチェック
+        if (!$request->has(['event_id', 'worker_id'])) {
+            return response([], 404);
+        }
+
+        // クエリビルダーを初期化
+        $query = Dispatch::query();
+
+        // 条件を追加
+        $query->where('event_id', $request->event_id)
+            ->where('worker_id', $request->worker_id);
+
+        // 結果を取得
+        $dispatch = $query->first();
+
+        // データが存在しない場合はエラー
+        if (!$dispatch) {
+            return response([], 404);
+        }
+
+        // 承諾フラグを更新して保存
+        $dispatch->approval = true;
+        $dispatch->save();
+
+        return response([], 204);
     }
 
     /**
