c1cbedf51fff2949ba2c0b73b0f28913c437727c
HondaYt <honda.yuto.04@gmail.com>
2024-11-20 01:44:11 +0900

    react-router-domでのrouting

--- 2024gorin-react-2/src/main.jsx
+++ 2024gorin-react-2/src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import Root from "./routers/root.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                index: true,
+                element: <App />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
